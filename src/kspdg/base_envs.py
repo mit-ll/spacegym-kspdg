fa1c06724187c1965f7941dcb6cf22b9c249e6aa
Ross Allen <ross.allen@ll.mit.edu>
2023-01-27 09:04:48 -0800

    starting abc kspdg envs

---
+++ src/kspdg/base_envs.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
+# Abstract base classes for KSPDG environment classes
+
