cdace54dbfc31bf9d6107fcad2c4f04dd1a531e9
Ross Allen <ross.allen@ll.mit.edu>
2023-01-30 08:43:22 -0800

    moving constants to new module

---
+++ src/kspdg/utils/constants.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
+# library-wide constant parameters
+
+from types import SimpleNamespace
+
+G0 = 9.80665 # standard gravity [m/s/s]
+
