74e58c1bf6aab66040494c0447f37353b788b278
Ross Allen <ross.allen@ll.mit.edu>
2023-02-02 11:26:02 -0800

    breaking heuristic policy into funcs

--- src/kspdg/lbg1/lg1_envs.py
+++ src/kspdg/lbg1/lg1_envs.py
@@ -43,64 +43,20 @@ class LBG1_LG1_ParentEnv(LadyBanditGuardGroup1Env):
 
         while not self.stop_bot_thread: 
 
-            print("DEBUG: GUARD: Performing direct pursuit of Bandit...")
-            loop_start_time = time.time()
-            while True:
+            # Step 1: Zero-out relative velocity between bandit and guard
+            self.zeroout_bandit_guard_velocity()
 
-                # get velocity and speed of bandit relative to guard
-                vel_vesB_vesG__lhgntw = self.vesBandit.velocity(self.vesGuard.orbital_reference_frame)
-                spd_vesB_vesG = np.linalg.norm(vel_vesB_vesG__lhgntw)
-                print("DEBUG: Bandit-Guard relative speed = ",spd_vesB_vesG)
+            if self.stop_bot_thread:
+                break
 
-                # check zero-ing vel breakout conditions to move to next step
-                if (spd_vesB_vesG < LBG1_LG1_ParentEnv.BG_MIN_REL_SPEED_THESHOLD) or \
-                    time.time() - loop_start_time > LBG1_LG1_ParentEnv.LOOP_TIMEOUT:
-                    break
+            # Step 2: Direct-pointing pursuit burn of guard toward bandit
+            self.direct_burn_guard_toward_bandit()
 
-                # point at negative relative velocity vector and burn for fixed time
-                self.vesGuard.auto_pilot.target_direction = -np.array(vel_vesB_vesG__lhgntw)
-                self.vesGuard.control.forward = -LBG1_LG1_ParentEnv.BG_PURSUIT_THROTTLE
-                time.sleep(0.2)
+            if self.stop_bot_thread:
+                break
 
-            print("DEBUG: GUARD: Performing direct pursuit of Bandit...")
-            loop_start_time = time.time()
-            while True:
-
-                # get velocity and speed of bandit relative to guard
-                vel_vesB_vesG__lhgntw = self.vesBandit.velocity(self.vesGuard.orbital_reference_frame)
-                spd_vesB_vesG = np.linalg.norm(vel_vesB_vesG__lhgntw)
-                print("DEBUG: Bandit-Guard relative speed = ",spd_vesB_vesG)
-
-                # check direct pursuit burn breakout conditions to move to next step
-                if (spd_vesB_vesG > LBG1_LG1_ParentEnv.BG_MAX_REL_SPEED_THESHOLD) or \
-                    time.time() - loop_start_time > LBG1_LG1_ParentEnv.LOOP_TIMEOUT:
-                    break
-
-                # Point at Bandint and burn for fixed time
-                pos_vesB_vesG__lhgntw = self.vesBandit.position(self.vesGuard.orbital_reference_frame)
-                self.vesGuard.auto_pilot.target_direction = pos_vesB_vesG__lhgntw
-                self.vesGuard.control.forward = LBG1_LG1_ParentEnv.BG_PURSUIT_THROTTLE
-                time.sleep(0.2)
-
-            self.vesGuard.control.forward = 0
-            print("DEBUG: GUARD: Coasting toward Bandit...")
-            loop_start_time = time.time()
-            while True:
-
-                # compute angle between relative velocity and relative position
-                # vectors for the Bandit-Guard system
-                vel_vesB_vesG__lhgntw = self.vesBandit.velocity(self.vesGuard.orbital_reference_frame)
-                pos_vesB_vesG__lhgntw = self.vesBandit.position(self.vesGuard.orbital_reference_frame)
-                ang_vel_pos_rad = U.angle_between(
-                    -np.array(vel_vesB_vesG__lhgntw), 
-                    np.array(pos_vesB_vesG__lhgntw))
-
-                print("DEBUG: Bandit-Guard vel-pos vector angle = ",ang_vel_pos_rad)
-
-                # check for coast breakout condition to repeat process
-                if ang_vel_pos_rad > LBG1_LG1_ParentEnv.BG_VEL_POS_ANG_THRESHOLD or \
-                    time.time() - loop_start_time > LBG1_LG1_ParentEnv.LOOP_TIMEOUT:
-                    break
+            # Step 3: Coast until relative velocity and position come out of alignment
+            self.coast_until_pos_vel_misalign()
         
         # terminate throttle
         self.vesGuard.control.forward = 0.0
@@ -108,30 +64,101 @@ class LBG1_LG1_ParentEnv(LadyBanditGuardGroup1Env):
         self.vesGuard.control.up = 0.0
         self.vesGuard.auto_pilot.disengage()
 
-    @staticmethod
-    def compute_target_pointing_angles(vesEgo, vesTarg) -> Tuple[float, float]:
-        """compute autopilot pitch and heading angles to point at a target vessel
+    def zeroout_bandit_guard_velocity(self):
+        """Perform burn to zero-out relative velocity between bandit and guard"""
 
-        Args:
-            vesEgo : krpc.types.Vessel
-                the vessel performing the targeting. Note this is potentially 
-                different from krpc's active_vessel which is the vessel on which
-                the gui is focused
-            vesTarg : krpc.types.Vessel
-                the vessel being targeted. Note that this is potentially
-                different from krpc's target_vessel. Changing the target_vessel
-                in krpc would affect behavior of active_vessel
+        print("DEBUG Zeroing: Zeroing Out Relative Velocities")
+        loop_start_time = time.time()
+        while True:
+
+            # get velocity and speed of bandit relative to guard
+            vel_vesB_vesG__lhgntw = self.vesBandit.velocity(self.vesGuard.orbital_reference_frame)
+            spd_vesB_vesG = np.linalg.norm(vel_vesB_vesG__lhgntw)
+            print("DEBUG Zeroing: Bandit-Guard relative speed = ",spd_vesB_vesG)
+
+            # check zero-ing vel breakout conditions to move to next step
+            if (spd_vesB_vesG < LBG1_LG1_ParentEnv.BG_MIN_REL_SPEED_THESHOLD) or \
+                time.time() - loop_start_time > LBG1_LG1_ParentEnv.LOOP_TIMEOUT or \
+                self.stop_bot_thread:
+                break
+
+            # point at negative relative velocity vector and burn for fixed time
+            self.vesGuard.auto_pilot.target_direction = -np.array(vel_vesB_vesG__lhgntw)
+            self.vesGuard.control.forward = -LBG1_LG1_ParentEnv.BG_PURSUIT_THROTTLE
+            time.sleep(0.2)
+
+    def direct_burn_guard_toward_bandit(self):
+        """Point Guard directly at Bandit and burn until desired relative velocity met"""
+
+        print("DEBUG DirectBurn: Performing direct pursuit of Bandit...")
+        loop_start_time = time.time()
+        while True:
+
+            # get velocity and speed of bandit relative to guard
+            vel_vesB_vesG__lhgntw = self.vesBandit.velocity(self.vesGuard.orbital_reference_frame)
+            spd_vesB_vesG = np.linalg.norm(vel_vesB_vesG__lhgntw)
+            print("DEBUG DirectBurn: Bandit-Guard relative speed = ",spd_vesB_vesG)
+
+            # check direct pursuit burn breakout conditions to move to next step
+            if (spd_vesB_vesG > LBG1_LG1_ParentEnv.BG_MAX_REL_SPEED_THESHOLD) or \
+                time.time() - loop_start_time > LBG1_LG1_ParentEnv.LOOP_TIMEOUT or\
+                self.stop_bot_thread:
+                break
+
+            # Point at Bandint and burn for fixed time
+            pos_vesB_vesG__lhgntw = self.vesBandit.position(self.vesGuard.orbital_reference_frame)
+            self.vesGuard.auto_pilot.target_direction = pos_vesB_vesG__lhgntw
+            self.vesGuard.control.forward = LBG1_LG1_ParentEnv.BG_PURSUIT_THROTTLE
+            time.sleep(0.2)
+
+    def coast_until_pos_vel_misalign(self):
+        """Coast until the relative position and relative velocity of Bandit-Guard misalign"""
+
+        self.vesGuard.control.forward = 0
+        print("DEBUG Coast: Coasting toward Bandit...")
+        # loop_start_time = time.time()
+        while True:
+
+            # compute angle between relative velocity and relative position
+            # vectors for the Bandit-Guard system
+            vel_vesB_vesG__lhgntw = self.vesBandit.velocity(self.vesGuard.orbital_reference_frame)
+            pos_vesB_vesG__lhgntw = self.vesBandit.position(self.vesGuard.orbital_reference_frame)
+            ang_vel_pos_rad = U.angle_between(
+                -np.array(vel_vesB_vesG__lhgntw), 
+                np.array(pos_vesB_vesG__lhgntw))
+
+            print("DEBUG Coast: Bandit-Guard vel-pos vector angle = ",ang_vel_pos_rad)
+
+            # check for coast breakout condition to repeat process
+            if ang_vel_pos_rad > LBG1_LG1_ParentEnv.BG_VEL_POS_ANG_THRESHOLD or \
+                self.stop_bot_thread:
+                # time.time() - loop_start_time > LBG1_LG1_ParentEnv.LOOP_TIMEOUT:
+                break
+
+    # @staticmethod
+    # def compute_target_pointing_angles(vesEgo, vesTarg) -> Tuple[float, float]:
+    #     """compute autopilot pitch and heading angles to point at a target vessel
+
+    #     Args:
+    #         vesEgo : krpc.types.Vessel
+    #             the vessel performing the targeting. Note this is potentially 
+    #             different from krpc's active_vessel which is the vessel on which
+    #             the gui is focused
+    #         vesTarg : krpc.types.Vessel
+    #             the vessel being targeted. Note that this is potentially
+    #             different from krpc's target_vessel. Changing the target_vessel
+    #             in krpc would affect behavior of active_vessel
 
 
-        Returns:
-            target_pitch : float
-                The target pitch, in degrees, between -90° and +90°
-            target_heading : 
-                The target heading, in degrees, between 0° and 360°.
-        """
+    #     Returns:
+    #         target_pitch : float
+    #             The target pitch, in degrees, between -90° and +90°
+    #         target_heading : 
+    #             The target heading, in degrees, between 0° and 360°.
+    #     """
 
-        # get position of target in ego's reference frame
-        p_vesTarg_vesEgo__lhEgoBody = vesTarg.position(vesEgo.reference_frame)
+    #     # get position of target in ego's reference frame
+    #     p_vesTarg_vesEgo__lhEgoBody = vesTarg.position(vesEgo.reference_frame)
 
 class LBG1_LG1_I1_Env(LBG1_LG1_ParentEnv):
     INIT_LOADFILE = "lbg1_i1_init"
