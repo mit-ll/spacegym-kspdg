fc48134a10fb1535d0ab657c46d82af9dcedf0c4
Ross Allen <ross.allen@ll.mit.edu>
2024-01-24 11:35:27 -0800

    updating copyright in top level

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
+- Updated copyright year in headers
+
 ### Removed
 
 - KSPDG Challenge announcement from README
--- scripts/example_agent_runner.py
+++ scripts/example_agent_runner.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Copyright (c) 2024, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
 # Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
 # SPDX-License-Identifier: MIT
 
