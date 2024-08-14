4221d430e051cbf22d90aa59b5c679bd91568cc0
Ross Allen <ross.allen@ll.mit.edu>
2024-08-13 21:32:52 -0700

    restructuring evaluate.py to use private_src

---
+++ evaluation/evaluate.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
+# calls private-source evaluate.py based upon system python version
+
+import sys
+
+if sys.version_info[:2] == (3, 12):
+    # Python 3.12
+    from kspdg.private_src.python3_12.kspdg_envs.dist_evaluate import main
+elif sys.version_info[:2] == (3, 9):
+    # Python 3.9
+    from kspdg.private_src.python3_9.kspdg_envs.dist_evaluate import main
+else:
+    # Handle other versions or raise an error
+    raise ImportError(f"evaluate.py requires python 3.9 or 3.12, got {sys.version}")
+
+if __name__ == "__main__":
+    main()
