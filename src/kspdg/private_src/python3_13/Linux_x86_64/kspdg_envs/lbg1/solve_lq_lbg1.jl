b9a1f9ccdbdb8499593d747833ae3e17633d5742
Ross Allen <ross.allen@ll.mit.edu>
2025-12-01 10:22:48 -0800

    adding solve_lq_lbg1.jl module

---
+++ src/kspdg/private_src/python3_13/Linux_x86_64/kspdg_envs/lbg1/solve_lq_lbg1.jl
@@ -0,0 +1,11 @@
+# Copyright (c) 2025, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
+"""
+solve_lq_lbg1.jl 
+
+find equilibrium strategies for bandit and guard in linear-quadratic 
+    lady-bandit-guard orbital problem
+
+"""
